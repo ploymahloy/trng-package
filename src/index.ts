a21ca75f4a94468bc4fc8985b816507e992c5e5f
ploymahloy <patrick.mahloy@gmail.com>
2026-04-25 17:13:58 -0400

    switch statement for returning random value;

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const message: string = "Hello from Node + TypeScript!";
+const getHighResTime = () => {
+	switch (true) {
+		case typeof process !== 'undefined' && !!process.hrtime?.bigint:
+			return process.hrtime.bigint();
+		case typeof performance !== 'undefined' && !!performance.now:
+			return BigInt(Math.floor(performance.now() * 1000000));
+		default:
+			return BigInt(Date.now() * 1000000);
+	}
+};
 
-console.log(message);
+console.log(getHighResTime());
